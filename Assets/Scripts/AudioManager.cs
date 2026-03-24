1ab37371533231374b6f33734467260a0fa53d82
August <goosey9999@gmail.com>
2026-03-24 16:27:01 -0400

    Added Audio Manager

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+
+public class AudioManager : MonoBehaviour
+{
+    [Header("Audio Sources")]
+    public AudioSource background;
+    public AudioSource player;
+
+    [Header("Audio Clips")]
+    public AudioClip goodClick;
+
+    public AudioClip badClick;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    void PlayMusic(){
+        
+
+    }
+
+    public void GoodClick(){
+        player.clip = goodClick;
+        player.Play();
+    }
+
+     public void BadClick(){
+        player.clip = badClick;
+        player.Play();
+    }
+
+}
